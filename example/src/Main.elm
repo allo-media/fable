f0ca4a7eb3c53303cf3ee2eaa9e5fb997d7a0be2
Fabrice Bentz <fabrice@weeple.fr>
2018-11-13 22:47:37 +0100

    Add real example with some function view.

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,10 +1,10 @@
 module Main exposing (main)
 
-import Css exposing (..)
 import Fable as Fable exposing (Book)
-import Html.Styled exposing (Html, button, div, h1, input, text)
-import Html.Styled.Attributes exposing (css)
-import Html.Styled.Events exposing (..)
+import Html.Styled exposing (text)
+import Ui.Button as Button
+import Ui.Input as Input
+import Ui.Panel as Panel
 
 
 type Msg
@@ -13,13 +13,30 @@ type Msg
 
 main : Book Msg
 main =
-    let
-        chapters =
-            [ Fable.chapter "Chapter 1"
-                [ Fable.story "Story"
-                    [ Fable.ui "ui-1" (div [] [ h1 [] [ text "test" ] ])
-                    ]
+    Fable.app
+        [ Fable.chapter "Forms"
+            [ Fable.story "Inputs"
+                [ Fable.ui "Default" (Input.default [] [])
+                , Fable.ui "Small" (Input.small [] [])
+                ]
+            , Fable.story "Button"
+                [ Fable.ui "Default" (Button.default [] [ text "Default" ])
+                , Fable.ui "Multiple"
+                    (Button.multiple []
+                        [ Button.default [] [ text "Default" ]
+                        , Button.primary [] [ text "Sign in" ]
+                        , Button.danger [] [ text "Alert ! This button delete internet !" ]
+                        , Button.warning [] [ text "Warning !" ]
+                        ]
+                    )
+                ]
+            , Fable.story "Panel"
+                [ Fable.ui "Default"
+                    (Panel.default []
+                        [ Panel.head [] [ text "Donec consequat diam" ]
+                        , Panel.content [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tortor nec leo vestibulum varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras elementum odio eu lectus iaculis pretium. Nulla vel velit venenatis, hendrerit nisl sit amet, placerat erat. Duis consectetur ante felis, ac auctor diam maximus vel. Phasellus scelerisque justo arcu, et dignissim ex laoreet non. Aenean sit amet sem sit amet eros aliquam placerat. Maecenas id viverra risus. Mauris tempor dui non commodo ornare. Mauris congue dignissim dolor quis auctor. Quisque eu erat orci. Mauris quis pretium justo, vitae gravida diam.\n\nDonec consequat diam mattis pellentesque tempus. Maecenas et interdum nisl, sit amet molestie felis. Donec euismod rhoncus enim, vel facilisis enim finibus vel. Sed sit amet diam leo. Fusce tincidunt eros quis imperdiet feugiat. Aliquam facilisis tellus fermentum tellus malesuada, ac sagittis metus volutpat. Cras eget interdum lorem. Praesent dignissim tellus eu venenatis sollicitudin. In sed volutpat mauris. Nulla venenatis sed augue sed iaculis.\n\n" ]
+                        ]
+                    )
                 ]
             ]
-    in
-    Fable.app chapters
+        ]
